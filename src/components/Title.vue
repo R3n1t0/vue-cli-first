40908b4e65d6bfd9e7e388378cd98872dd1d3a59
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-12 15:51:31 +0200

    un po di problemi

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>“La mia prima app con Vue CLI!”</h1>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<style lang="scss">
+  h1{
+    margin: 0 auto;
+    color:#ff3b44;
+  }
+</style>
